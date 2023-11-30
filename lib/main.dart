cee67dc74436ec5d72b76207b4ee2fe008176cb5
ky.song <thdrldud369@gmail.com>
2023-11-30 21:15:00 +0900

    🐛 [Fix] 수정: Hot Reloading 버그 수정
    runApp() 함수에서 직접 위젯 실행시 Hot Reloading이 동작하지 않는 버그 수정

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,20 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(
-    home: Scaffold(body: Text("Hello World")),
-  ));
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext contest) {
+    return (MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: Text(
+            "Hello World12",
+          ),
+        ),
+      ),
+    ));
+  }
 }
